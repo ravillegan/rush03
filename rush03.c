f44efa3bd47ccce7b8b77a27fe8931921051f2f1
Adina Santiago <asantiag@e3r2p16.21-school.ru>
2019-03-02 12:29:42 +0300

    2

--- rush03.c
+++ rush03.c
@@ -1,28 +1,28 @@
-extern void ft_putchar(char c);
+extern	void	ft_putchar(char c);
 
-void    rush(int x, int y)
+void			rush(int x, int y)
 {
-    int i;
-    int j;
-    int mat[x][y];
+	int i;
+	int j;
+	int mat[x][y];
 
-    i = 1;
-    while (i <= x)
-    {
-        j = 1;
-        while (j <= y)
-        {
-            if ((i == 1 && j == 1) || (i == x && j == 1))
-                ft_putchar('A');
-            if (((i == 1 && j == y) && j != 1) || ((i == x && j == y) && j != 1))
-                ft_putchar('C');
-            if (((i > 1 && i < x) && j == 1) || ((i == 1 && j > 1) && j < y) || ((i > 1 && i < x) && j == y) || ((i == x && j > 1) && j < y))
-                ft_putchar('B');
-            if ((i > 1 && i < x) && (j > 1 & j < y))
-                ft_putchar(' ');
-            j++;
-        }
-    ft_putchar('\n');
-    i++;
-    }
+	i = 1;
+	while (i <= x)
+	{
+		j = 1;
+		while (j <= y)
+		{
+			if (j == 1 && (i == 1 || i == x))
+				ft_putchar('A');
+			if ((j == y && j != 1) && (i == 1 || i == x))
+				ft_putchar('C');
+			if (((i > 1 && i < x) && j == 1) || ((i == 1 && j > 1) && j < y) || ((i > 1 && i < x) && j == y) || ((i == x && j > 1) && j < y))
+				ft_putchar('B');
+			if ((i > 1 && i < x) && (j > 1 & j < y))
+				ft_putchar(' ');
+			j++;
+		}
+		ft_putchar('\n');
+		i++;
+	}
 }
